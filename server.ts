29fc2028683bd31e07d12329c153708c0131d23b
Aldair Avelino <aldair03avelino@gmail.com>
2025-11-16 22:55:52 +0100

    calling users routes

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import userRoutes from "@routes/user.route"
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.json());
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
